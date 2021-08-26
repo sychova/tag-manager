e1323202c2401ab45d091be70d9d189cb83387ce
sychova <anastasiya.sychova@gmail.com>
2021-08-27 00:26:59 +0300

    fix: fixing

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@
         }
     });
 
-    function generateID(arr) {
+    let generateID = function(arr) {
         var tagID = Math.floor(Math.random() * 100).toString();
         var a = arr.includes(tagID);
         if (a) {
@@ -16,32 +16,31 @@
         }
     };
 
-    function addTag(tagNames) {
-        let list = document.getElementById("tagList");
+    let addTag = function(tagNames) {
         let tagName = tagNames.split(",");
         if (tagName.length == 1) {
             let tagID = generateID(Object.keys(localStorage));
             localStorage.setItem(tagID, tagName);
-            list += tagBuilder(tagName, tagID);
+            tagListBuilder();
         } else {
             for (var i = 0; i < tagName.length; i++) {
                 let tagID = generateID(Object.keys(localStorage));
                 localStorage.setItem(tagID, tagName[i]);
             }
-            list += tagBuilder(tagName, tagID);
+            tagListBuilder();
         }
     }
 
-    function deleteTag(tagID) {
+    let deleteTag = function(tagID) {
         localStorage.removeItem(tagID);
         tagListBuilder();
     }
 
-    function tagBuilder(tagName, tagID) {
+    let tagBuilder = function(tagName, tagID) {
         return `<div class="tag"><span>${tagName}</span><a class="delete" tagID="${tagID}">&#10005</a></div>`
     }
 
-    function tagListBuilder() {
+    let tagListBuilder = function() {
         let list = document.getElementById("tagList");
         list.innerHTML = "";
         keys = Object.keys(localStorage);
