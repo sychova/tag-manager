bf17f799436f093df50aceee6cf1de4443668a90
sychova <anastasiya.sychova@gmail.com>
2021-08-26 23:01:21 +0300

    init: added html, css and js for the tag manager

---
+++ scripts.js
@@ -0,0 +1,66 @@
+(function() {
+    document.getElementById("addTag").addEventListener("click", function() {
+        let tagNames = document.getElementById("tagName").value;
+        if (tagNames.length > 0) {
+            addTag(tagNames);
+        }
+    });
+
+    function generateID(arr) {
+        var tagID = Math.floor(Math.random() * 100).toString();
+        var a = arr.includes(tagID);
+        if (a) {
+            return generateID(arr);
+        } else {
+            return tagID;
+        }
+    };
+
+    function addTag(tagNames) {
+        let list = document.getElementById("tagList");
+        let tagName = tagNames.split(",");
+        if (tagName.length == 1) {
+            let tagID = generateID(Object.keys(localStorage));
+            localStorage.setItem(tagID, tagName);
+            tagListBuilder();
+        } else {
+            for (var i = 0; i < tagName.length; i++) {
+                let tagID = generateID(Object.keys(localStorage));
+                localStorage.setItem(tagID, tagName[i]);
+            }
+            tagListBuilder();
+        }
+    }
+
+    function deleteTag(tagID) {
+        localStorage.removeItem(tagID);
+        tagListBuilder();
+    }
+
+    function tagBuilder(tagName, tagID) {
+        return `<div class="tag"><span>${tagName}</span><a class="delete" tagID="${tagID}">&#10005</a></div>`
+    }
+
+    function tagListBuilder() {
+        let list = document.getElementById("tagList");
+        list.innerHTML = "";
+        keys = Object.keys(localStorage);
+        if (keys.length > 0) {
+            for (i of keys) {
+                var objectTag = localStorage.getItem(i);
+                let item = tagBuilder(objectTag, i);
+                list.innerHTML += item;
+            }
+            let deleteBtns = document.getElementsByClassName("delete");
+            for (i of deleteBtns) {
+                let tagID = i.getAttribute("tagID");
+                i.addEventListener("click", function() {
+                    deleteTag(tagID);
+                });
+            }
+        } else {
+            list.innerHTML += "No tags available";
+        }
+    }
+    tagListBuilder();
+}());
